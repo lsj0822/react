8d2651b05529c584257ed64254d6c18377a0000a
lsj0822 <lsj0822@khu.ac.kr>
2024-01-15 00:01:16 +0900

    240114

--- sample-react-app/src/App.js
+++ sample-react-app/src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import Input from './Input';
-
+import { BrowserRouter, Route, Routes, Link, useNavigate } from 'react-router-dom';
 function App(){
-  return <Input/>
+  return (<BrowserRouter>
+  <button>
+    <Link to='/home'>Home</Link>
+  </button>
+    <button>
+    <Link to='/'>Index</Link>
+  </button>
+  <Routes>
+    <Route path="/" element={<Index/>}/>
+    <Route path="/home" element={<Home/>}/>
+  </Routes>
+  </BrowserRouter>
+  );
 }
 
+function Index(){
+  var navigate = useNavigate();
+  function handleClick(){
+    navigate("/home")
+  }
+
+  return (
+    <>
+      <h2>Index</h2>
+      <button onClick={handleClick}>Go Home</button>
+    </>
+  )
+}
+function Home(){
+  return (
+    <>
+      <h2>Home</h2>
+      <p>This is home</p>
+    </>
+  )
+}
 export default App;
